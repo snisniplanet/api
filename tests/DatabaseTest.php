b2560d17d4961ba995fc9269fee0a4c0897d69a9
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-05-09 14:43:00 +0200

    Added PHPUnit

---
+++ tests/DatabaseTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class DatabaseTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testConnection()
+    {
+        //
+    }
+}
