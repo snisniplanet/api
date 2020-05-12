11db08445cb436ebb3392ce36c7668300f5cb7ce
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-05-12 15:00:56 +0200

    Added Managers

---
+++ database/migrations/2020_05_12_125146_create_managers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateManagersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('managers', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('blog_id')->constrained();
+
+            $table->primary(['user_id', 'blog_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('managers');
+    }
+}
