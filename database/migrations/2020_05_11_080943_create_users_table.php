28a6cc028575088f62c87be85cb98069044baa8a
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-05-12 12:33:14 +0200

    Made unique username

--- database/migrations/2020_05_11_080943_create_users_table.php
+++ database/migrations/2020_05_11_080943_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email');
             $table->string('password');
             $table->timestamp('last_logged_in')->nullable();
