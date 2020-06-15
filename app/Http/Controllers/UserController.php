daa6e74642b85f7958d09a376952e8ef5c45ef53
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-06-15 17:09:45 +0200

    Fixed login input validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
     public function login(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'email' => 'required|email|unique:users',
+            'email' => 'required|email',
             'password' => 'required'
         ]);
 
