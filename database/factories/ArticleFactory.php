0dd9c057fc031b2e64834fc05b3e9df7251f14d9
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-07-20 11:57:20 +0200

    Title as sentence and snippet as paragraph

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Article::class, function (Faker $faker) {
     return [
-        "title" => $faker->word,
-        "snippet" => $faker->sentence,
+        "title" => $faker->sentence,
+        "snippet" => $faker->paragraph,
         "content" => '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"I would really like to make a blog"}]}]}',
         "blog_id" => App\Blog::all()->random()
     ];
