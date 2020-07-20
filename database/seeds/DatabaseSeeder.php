175bdd49ddf79eb10847c1c8c6b01ab780e43d69
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-07-20 15:46:47 +0200

    Removed article seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             'UserSeeder',
             'BlogSeeder',
-            'ArticleSeeder',
+            //'ArticleSeeder',
         ]);
     }
 }
